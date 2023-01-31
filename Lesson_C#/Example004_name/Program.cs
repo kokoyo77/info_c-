33b89f2150c8125d3b3ae211903dd2649eef2e21
artem <normandysr777@yandex.ru>
2023-01-31 15:13:53 +0300

    save1.2

---
+++ Lesson_C#/Example004_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Полина")
+{
+ Console.WriteLine("Ура! Это же ПОЛИНА");  
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
